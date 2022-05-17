72e55f2f172e4a836c8dae92281c23ba9f0d4ca6
Dylan Ashby <doops561@gmail.com>
2022-05-17 14:12:44 -0600

    tried again

--- Baseball Analytics/TestAnalytics.r
+++ Baseball Analytics/TestAnalytics.r
@@ -1,15 +1,15 @@
 # Read data
 # ON MY LAPTOP
-#batter_data
-#   <- read.csv(file = "C:/Users/Doops/Desktop/archive/Batting.csv")
-#fame_data
-#   <- read.csv(file = "C:/Users/Doops/Desktop/archive/HallOfFame.csv")
+batter_data <-
+    read.csv(file = "C:/Users/Doops/Desktop/archive/Batting.csv")
+fame_data <-
+    read.csv(file = "C:/Users/Doops/Desktop/archive/HallOfFame.csv")
 
 # ON MY DESKTOP
-batter_data <-
-    read.csv(file = "C:/Users/Dylan Ashby/Desktop/archive/Batting.csv")
-fame_data <-
-    read.csv(file = "C:/Users/Dylan Ashby/Desktop/archive/HallOfFame.csv")
+#batter_data <-
+#    read.csv(file = "C:/Users/Dylan Ashby/Desktop/archive/Batting.csv")
+#fame_data <-
+#    read.csv(file = "C:/Users/Dylan Ashby/Desktop/archive/HallOfFame.csv")
 
 # Ommit any NA in the batter data
 batter_data <- na.omit(batter_data)
@@ -23,10 +23,9 @@ inducted_data <- fame_data[which(fame_data$inducted == "Y"), ]
 
 #exclude_names <- list(inducted_data$playerID)
 exclude_names <- list(rownames(fame_data[which(fame_data$playerID %in% inducted_data$playerID), ]))
-#fame_data <- fame_data[which(rownames(fame_data) != exclude_names), ]
-fame_data2 <- fame_data[rownames(fame_data) = exclude_names,]
+fame_data <- fame_data[which(rownames(fame_data) != exclude_names), ]
 
-#fame_data2 <- fame_data[-c()]
+
 #print(fame_data2)
 
 not_data <- fame_data[which(fame_data$inducted == "N"), ]
@@ -47,4 +46,4 @@ total_not_mean <- mean(not_means$RBI)
 print(total_inducted_mean)
 print(total_not_mean)
 
-write.csv(fame_data2, "C:\\Users\\Dylan Ashby\\Desktop\\Output.csv", )
+write.csv(fame_data, "C:\\Users\\Dylan Ashby\\Desktop\\Output.csv", )
