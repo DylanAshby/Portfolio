e16e1079e78113c7d86e4f09212796382eb0a1be
Dylan Ashby <doops561@gmail.com>
2022-05-17 02:02:27 -0600

    Working on analystics

--- Baseball Analytics/HelloWorld.r
+++ Baseball Analytics/HelloWorld.r
@@ -15,3 +15,20 @@ paste(text1, text2)
 print(getwd())
 setwd("C:/Users/Dylan Ashby/Desktop/archive")
 print(getwd())
+
+# Create a list of dataframes where each dataframe is the
+# career stats of a hall of fame player
+#career_stat_blocks <- list()
+
+#for (player in inducted_data) {
+#    stat_block <-
+#    data.frame(inducted_batter[which(inducted_batter$playerID == player[0]), ])
+#    append(career_stat_blocks, stat_block, )
+#}
+
+#print(summary(inducted_batter))
+
+#inducted_batter2 <- inducted_batter[,-1]
+#rownames(inducted_batter2) <- inducted_batter[,1]
+
+#result <- aggregate(RBI ~ playerID, data = inducted_batter, paste, collapse = ",")
--- Baseball Analytics/TestAnalytics.r
+++ Baseball Analytics/TestAnalytics.r
@@ -1,11 +1,15 @@
 # Read data
 # ON MY LAPTOP
-#batter_data <- read.csv(file = "C:/Users/Doops/Desktop/archive/Batting.csv")
-#fame_data <- read.csv(file = "C:/Users/Doops/Desktop/archive/HallOfFame.csv")
+#batter_data
+#   <- read.csv(file = "C:/Users/Doops/Desktop/archive/Batting.csv")
+#fame_data
+#   <- read.csv(file = "C:/Users/Doops/Desktop/archive/HallOfFame.csv")
 
 # ON MY DESKTOP
-batter_data <- read.csv(file = "C:/Users/Dylan Ashby/Desktop/archive/Batting.csv")
-fame_data <- read.csv(file = "C:/Users/Dylan Ashby/Desktop/archive/HallOfFame.csv")
+batter_data <-
+    read.csv(file = "C:/Users/Dylan Ashby/Desktop/archive/Batting.csv")
+fame_data <-
+    read.csv(file = "C:/Users/Dylan Ashby/Desktop/archive/HallOfFame.csv")
 
 # Ommit any NA in the batter data
 batter_data <- na.omit(batter_data)
@@ -17,19 +21,30 @@ fame_data <- data.frame(fame_data)
 # Get all players actually inducted
 inducted_data <- fame_data[which(fame_data$inducted == "Y"), ]
 
+#exclude_names <- list(inducted_data$playerID)
+exclude_names <- list(rownames(fame_data[which(fame_data$playerID %in% inducted_data$playerID), ]))
+#fame_data <- fame_data[which(rownames(fame_data) != exclude_names), ]
+fame_data2 <- fame_data[rownames(fame_data) = exclude_names,]
+
+#fame_data2 <- fame_data[-c()]
+#print(fame_data2)
+
+not_data <- fame_data[which(fame_data$inducted == "N"), ]
+
 # Retrieve batting stats of the inducted players
 inducted_batter <-
 batter_data[which(batter_data$playerID %in% inducted_data$playerID), ]
 
+not_batter <-
+batter_data[which(batter_data$playerID %in% not_data$playerID), ]
 
-# Create a list of dataframes where each dataframe is the
-# career stats of a hall of fame player
-career_stat_blocks <- list()
+inducted_means <- aggregate(RBI ~ playerID, data = inducted_batter, mean)
+not_means <- aggregate(RBI ~ playerID, data = not_batter, mean)
 
-for (player in inducted_data) {
-    stat_block <-
-    data.frame(inducted_batter[which(inducted_batter$playerID == player[0]), ])
-    append(career_stat_blocks, stat_block, )
-}
+total_inducted_mean <- mean(inducted_means$RBI)
+total_not_mean <- mean(not_means$RBI)
 
-#print(summary(inducted_batter))
+print(total_inducted_mean)
+print(total_not_mean)
+
+write.csv(fame_data2, "C:\\Users\\Dylan Ashby\\Desktop\\Output.csv", )
