2d61e99d4684f14a923838891b231151e9ecf32a
Dylan Ashby <doops561@gmail.com>
2022-05-17 20:16:15 -0600

    Finished formatting the T-test. Added in testing for average player salary

--- Baseball Analytics/TestAnalytics.r
+++ Baseball Analytics/TestAnalytics.r
@@ -1,15 +1,10 @@
-# ON MY LAPTOP
+# Load In Dataset
 batter_data <-
-   read.csv(file = "C:/Users/Doops/Desktop/archive/Batting.csv")
+    read.csv(file = "C:/Users/Dylan Ashby/Desktop/archive/Batting.csv")
+salary_data <-
+    read.csv(file = "C:/Users/Dylan Ashby/Desktop/archive/Salaries.csv")
 fame_data <-
-   read.csv(file = "C:/Users/Doops/Desktop/archive/HallOfFame.csv")
-
-
-# # Load In Dataset
-# batter_data <-
-#     read.csv(file = "C:/Users/Dylan Ashby/Desktop/archive/Batting.csv")
-# fame_data <-
-#     read.csv(file = "C:/Users/Dylan Ashby/Desktop/archive/HallOfFame.csv")
+    read.csv(file = "C:/Users/Dylan Ashby/Desktop/archive/HallOfFame.csv")
 
 # Ommit any NA in the batter data
 batter_data <- na.omit(batter_data)
@@ -24,6 +19,10 @@ inducted_data <- fame_data[which(fame_data$inducted == "Y"), ]
 # Get all players that were not inducted
 not_data <- fame_data[which(fame_data$inducted == "N"), ]
 
+
+# QUESTION 1: From players nominated for the hall of fame, do the ones who
+#             are actually inducted score more RBI's than those who are not?
+
 # Retrieve batting stats of the inducted players
 inducted_batter <-
     batter_data[which(batter_data$playerID %in% inducted_data$playerID), ]
@@ -31,11 +30,6 @@ inducted_batter <-
 # Retrieve batting stats of the not inducted players
 not_batter <-
     batter_data[which(batter_data$playerID %in% not_data$playerID), ]
-
-
-
-
-# QUESTION 1: Do players
 
 # Find mean RBI for each player
 inducted_rbi_means <- aggregate(RBI ~ playerID, data = inducted_batter, mean)
@@ -45,5 +39,32 @@ not_rbi_means <- aggregate(RBI ~ playerID, data = not_batter, mean)
 not_rbi_means <- not_rbi_means[which(
     !not_rbi_means$playerID %in% inducted_rbi_means$playerID), ]
 
-# Perform Two-samples T-test
+# Perform Two Samples T-test
+print("TWO SAMPLE T-TEST ---------- RBI MEANS")
+print("Alpha value: 0.05")
 print(t.test(inducted_rbi_means$RBI, not_rbi_means$RBI, var.equal = FALSE))
+
+
+#Question 2: From players nominated to the hall of fame, do the ones who are
+#            actually inducted have a higher salary than those who are not?
+
+# Retrieve salary stats of the inducted players
+inducted_salary <-
+    salary_data[which(salary_data$playerID %in% inducted_data$playerID), ]
+
+# Retrieve salary stats of the not inducted players
+not_salary <-
+    salary_data[which(salary_data$playerID %in% not_data$playerID), ]
+
+# Find mean salary for each player
+inducted_sal_means <- aggregate(salary ~ playerID, data = inducted_salary, mean)
+not_sal_means <- aggregate(salary ~ playerID, data = not_salary, mean)
+
+# Exclude the names on the inducted list from the not inducted list
+not_sal_means <- not_sal_means[which(
+    !not_sal_means$playerID %in% inducted_sal_means$playerID), ]
+
+# Perform Two Samples T-test
+print("TWO SAMPLE T-TEST ---------- SALARY MEANS")
+print("Alpha value: 0.05")
+print(t.test(inducted_sal_means$salary, not_sal_means$salary, var.equal =FALSE))
