67831d5017736ecf8e923daca4e3bd193adcf56c
Dylan Ashby <doops561@gmail.com>
2022-06-04 17:30:29 -0600

    Commented out

--- Erlang/test.erl
+++ Erlang/test.erl
@@ -1,20 +1,20 @@
--module(test).
--export([run/0, merge/2, split/1]).
+% -module(test).
+% -export([run/0, merge/2, split/1]).
 
-run() ->
-    List1 = [9676, 288, 7215, 4277, 4639, 9495, 936, 7001, 666, 8849, 4596, 1551, 5729, 119, 6787, 4467, 5733, 5135, 2951],
+% run() ->
+%     List1 = [9676, 288, 7215, 4277, 4639, 9495, 936, 7001, 666, 8849, 4596, 1551, 5729, 119, 6787, 4467, 5733, 5135, 2951],
 
-    io:fwrite("~p\n", [split(List1)]).
+%     io:fwrite("~p\n", [split(List1)]).
 
-% sort(L) ->
-%     Result = if
-%         length(L) > 1 -> sort()
+% % sort(L) ->
+% %     Result = if
+% %         length(L) > 1 -> sort()
 
-merge(L1, L2) ->
-    L1 ++ L2.
+% merge(L1, L2) ->
+%     L1 ++ L2.
 
-split([Left | Right]) ->
-    Left.
+% split([Left | Right]) ->
+%     Left.
 
 % -module(test).
 % -export([printnum/0, num/1]).
